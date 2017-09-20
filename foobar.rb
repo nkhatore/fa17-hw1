e9749b819c81f77f93a98f164ff60ba8588db8ff
Nilay Khatore <khatorenilay@gmail.com>
2017-09-19 19:05:04 -0700

    finish questions

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,10 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    b = a.collect { |x| x.to_i + 2}
+    b.keep_if { |x| x % 2 == 0 }
+    b = b.uniq
+    b.delete_if { |x| x > 10 }
+    b.inject(0) { |sum, x| sum + x }
   end
 end
-
-
